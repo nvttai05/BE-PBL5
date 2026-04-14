fa437e0d786376c3ef84e135f3d87e8f696d95c3
tai <nvttai05@gmail.com>
2026-04-14 17:47:02 +0700

    code nay moi nhat do

--- app/core/config.py
+++ app/core/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     PORT: int = 8000
 
     #YOLO
-    YOLO_MODEL_PATH: str = "yolo11m.pt"
+    YOLO_MODEL_PATH: str = "best.pt"
     CONFIDENCE_THRESHOLD: float = 0.5
     IMGSZ: int = 640
 
