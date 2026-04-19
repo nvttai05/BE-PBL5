de562eaea38609af392ee5f6228bf4981699da35
unknown <huynhtuan2512>
2026-04-19 21:56:38 +0700

    finish learning-history

--- BE/app/schemas/__init__.py
+++ BE/app/schemas/__init__.py
@@ -1,13 +1,21 @@
 from .detection import DetectionResponse, DetectionItem
 from .speak import SpeakRequest, SpeakResponse
-from .history import HistoryResponse, HistoryItem
+from .history import (
+    HistoryItem,
+    HistoryListResponse,
+    HistoryCreateRequest,
+    HistoryDeleteResponse,
+    HistorySummaryResponse,
+    SessionType,
+)
 from .quiz import QuizRequest, QuizResponse, QuizQuestion
 from .common import ErrorResponse, StatusResponse
 
 __all__ = [
     "DetectionResponse", "DetectionItem",
     "SpeakRequest", "SpeakResponse",
-    "HistoryResponse", "HistoryItem",
+    "HistoryItem", "HistoryListResponse", "HistoryCreateRequest",
+    "HistoryDeleteResponse", "HistorySummaryResponse", "SessionType",
     "QuizRequest", "QuizResponse", "QuizQuestion",
-    "ErrorResponse", "StatusResponse"
+    "ErrorResponse", "StatusResponse",
 ]
